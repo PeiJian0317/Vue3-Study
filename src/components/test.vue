3e3304849558ae3ee6d560aa833b62409f38568a
PeiJian <2567750155@qq.com>
2023-11-29 21:42:11 +0800

    v3中的监视属性 --- 监视ref定义的数据

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,28 +1,34 @@
 <template>
-  姓:<input type="text" v-model="person.firstName">
-  名:<input type="text" v-model="person.lastName">
-  <h2>{{ person.fullname }}</h2>
+ <h2>当前求和为:{{ sum }}</h2>
+ <button @click="sum++">点击加</button>
+ <hr>
+ <h2>当前文字为:{{ msg }}</h2>
+ <button @click="msg += '!'">点击修改文字</button>
 </template>
 
 <script>
-import { reactive,computed } from 'vue';
+import { ref, watch } from 'vue';
 export default {
   name: "test",
   setup() {
-    //数据
-    let person = reactive({
-      firstName:"张",
-      lastName:"三"
-    })
+    let sum = ref(0)
+    let msg = ref('测试')
 
-    person.fullname = computed(() =>{
-      return person.firstName + person.lastName
+    //情况一:监视一个ref定义的响应式数据时
+    // watch(sum,(newValue,oldValue) =>{
+    //   console.log(newValue,oldValue)
+    // })
+
+    //情况二:监视多个ref定义的响应式数据时 --->写成数组形式
+    watch([sum,msg],(newValue,oldValue) =>{
+      console.log(newValue,oldValue)
     })
 
 
     // 需要把结果返回出去(返回的是一个对象)
     return {
-      person,
+      sum,
+      msg
     };
 
   },
