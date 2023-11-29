c9155b782b1439b1f4582fe18fdf08c170bc2843
PeiJian <2567750155@qq.com>
2023-11-29 15:20:04 +0800

    利用reactive实现数据响应式

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,40 +1,50 @@
 <template>
-  <h2>{{ name }}</h2>
-  <h2>{{ age }}</h2>
+  <h2>{{ person.name }}</h2>
+  <h2>{{ person.age }}</h2>
   <button @click="ChangeInfo">修改人的信息</button>
   <button @click="ChangAge">修改人的年龄</button>
-  <h3>工作种类:{{ job.type }}</h3>
-  <h3>工资薪水:{{ job.salary }}</h3>
+  <h3>工作种类:{{ person.type }}</h3>
+  <h3>工资薪水:{{ person.salary }}</h3>
+  <h3>爱好是:{{ hobby }}</h3>
+  <h3>学生性别是:{{ person.student.sex }}</h3>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { reactive } from 'vue';
 export default {
   name: "test",
   setup() {
     //数据
-    let name = ref("张三");
-    let age = ref(19);
-    let job = ref({
-        type:"工程师",
-        salary:"30k"
+    let person = reactive({
+      //数据
+      name:'张三',
+      age:19,
+      type:"工程师",
+      salary:'30k',
+      student:{
+        sex:'女'
+      }
     })
 
+    let hobby = reactive(['抽烟','喝酒','烫头'])
+
     function ChangeInfo(){
-        job.value.type = '设计师',
-        job.value.salary = '35k'
+        person.type = '设计师',
+        person.salary = '35k'
+        hobby[0] = '学习'
     }
     function ChangAge() {
-      age.value = 20
+      person.age = 20
     }
+
+
+
     // 需要把结果返回出去(返回的是一个对象)
     return {
-      name,
-      age,
-      job,
+      person,
       ChangAge,
       ChangeInfo,
-      
+      hobby
     };
 
   },
