f5e7983f6531f0d04fab251a40c0ed39d5dcc020
PeiJian <2567750155@qq.com>
2023-12-02 16:23:13 +0800

    hooks自定义函数

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,34 +1,17 @@
 <template>
- <h2>当前求和为:{{ sum }}</h2>
- <button @click="sum++">点击加</button>
- <hr>
- <h2>当前文字为:{{ msg }}</h2>
- <button @click="msg += '!'">点击修改文字</button>
+  <h2>当前点击时鼠标的坐标为:x: {{ point.x }},y: {{ point.y }}</h2>
 </template>
 
 <script>
-import { ref, watch } from 'vue';
+import usePoint from "../hooks/usePoint";
 export default {
   name: "test",
   setup() {
-    let sum = ref(0)
-    let msg = ref('测试')
-
-    //情况一:监视一个ref定义的响应式数据时
-    // watch(sum,(newValue,oldValue) =>{
-    //   console.log(newValue,oldValue)
-    // })
-
-    //情况二:监视多个ref定义的响应式数据时 --->写成数组形式
-    watch([sum,msg],(newValue,oldValue) =>{
-      console.log(newValue,oldValue)
-    })
-
+    const {point} = usePoint();
 
     // 需要把结果返回出去(返回的是一个对象)
     return {
-      sum,
-      msg
+      point
     };
 
   },
